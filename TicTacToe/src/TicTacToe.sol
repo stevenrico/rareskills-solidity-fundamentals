8d07519b0020d99768a542d4108750bddb903341
Steven Rico <dev.stevenrico@proton.me>
2023-06-11 11:30:46 +0100

    Nested Arrays Practice Complete.

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,48 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        return _checkDiagonalWin(board) || _checkRowWin(board) || _checkColWin(board);
+    }
+
+    function _checkDiagonalWin(uint8[3][3] memory board) private pure returns (bool) {
+        uint8[3] memory firstRow = board[0];
+        uint8[3] memory secondRow = board[1];
+        uint8[3] memory thirdRow = board[2];
+
+        if (firstRow[0] == secondRow[1] && firstRow[0] == thirdRow[2]) {
+            return true;
+        }
+
+        if (firstRow[2] == secondRow[1] && firstRow[2] == thirdRow[0]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function _checkRowWin(uint8[3][3] memory board) private pure returns (bool) {
+        for (uint256 i = 0; i < board.length; i++) {
+            uint8[3] memory row = board[i];
+
+            if (row[0] == row[1] && row[0] == row[2]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function _checkColWin(uint8[3][3] memory board) private pure returns (bool) {
+        uint8[3] memory firstRow = board[0];
+        uint8[3] memory secondRow = board[1];
+        uint8[3] memory thirdRow = board[2];
+
+        for (uint256 i = 0; i < firstRow.length; i++) {
+            if (firstRow[i] == secondRow[i] && firstRow[i] == thirdRow[i]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
