8208f540150e855a26ef6b9c4f212ec5585d91bb
Steven Rico <dev.stevenrico@proton.me>
2023-06-11 14:13:12 +0100

    Msg.sender Practice Complete.

--- WhoCalledMe/src/WhoCalledMe.sol
+++ WhoCalledMe/src/WhoCalledMe.sol
@@ -5,7 +5,6 @@ contract WhoCalledMe {
     address public caller;
 
     function callMe() public {
-        // use msg.sender to store the address
-        // of the person who called this contract
+        caller = msg.sender;
     }
 }
