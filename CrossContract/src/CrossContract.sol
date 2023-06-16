b30a767f3bbcb6ce457f986515d27c6c286206a6
Steven Rico <dev.stevenrico@proton.me>
2023-06-17 00:28:42 +0100

    Call Other Contracts Practice Complete.

--- CrossContract/src/CrossContract.sol
+++ CrossContract/src/CrossContract.sol
@@ -6,11 +6,22 @@ contract CrossContract {
      * The function below is to call the price function of PriceOracle1 and PriceOracle2 contracts below and return the lower of the two prices
      */
 
-    function getLowerPrice(
-        address _priceOracle1,
-        address _priceOracle2
-    ) external view returns (uint256) {
-        // your code here
+    function getLowerPrice(address _priceOracle1, address _priceOracle2) external returns (uint256) {
+        (bool ok1, bytes memory result1) = _priceOracle1.call(abi.encodeWithSignature("price()"));
+        require(ok1, "call failed");
+
+        uint256 price1 = abi.decode(result1, (uint256));
+
+        (bool ok2, bytes memory result2) = _priceOracle2.call(abi.encodeWithSignature("price()"));
+        require(ok2, "call failed");
+
+        uint256 price2 = abi.decode(result2, (uint256));
+
+        if (price1 < price2) {
+            return price1;
+        } else {
+            return price2;
+        }
     }
 }
 
