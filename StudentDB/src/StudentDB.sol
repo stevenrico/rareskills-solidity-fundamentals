42f981b17fd6224d0dd4f440bb438347ef1ca895
Steven Rico <dev.stevenrico@proton.me>
2023-06-19 16:08:32 +0100

    Struct Practice Complete.

--- StudentDB/src/StudentDB.sol
+++ StudentDB/src/StudentDB.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.13;
 
 contract StudentDB {
     /* This exercise assumes you know how structs works.
-   create a struct `Student` for a student who's name is `John` and age is `12`.
-   Return John's age in the function below */
+    create a struct `Student` for a student who's name is `John` and age is `12`.
+    Return John's age in the function below */
 
     struct Student {
         string name;
         uint256 age;
     }
+
+    Student public student;
 
     constructor() {
         createStudent("John", 12);
@@ -17,11 +19,11 @@ contract StudentDB {
 
     // create Student's data
     function createStudent(string memory _name, uint256 _age) public {
-        // your code here
+        student = Student(_name, _age);
     }
 
     // return struct data
     function getEntireStruct() public view returns (Student memory) {
-        // your code here
+        return student;
     }
 }
